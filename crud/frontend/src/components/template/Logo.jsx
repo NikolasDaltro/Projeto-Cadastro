dcf26ab2e0abe5963f359cf608c2389faf7db85b
NikolasDaltro <nikolasdsantos@gmail.com>
2022-10-31 15:12:31 -0300

    Bug Logo

--- crud/frontend/src/components/template/Logo.jsx
+++ crud/frontend/src/components/template/Logo.jsx
@@ -1,7 +1,10 @@
 import './Logo.css'
+import logo from '../../assets/imgs/logo.png'
 import React from 'react'
 
 export default props =>
-        <asside className="logo">
-            Logo
-        </asside>
+        <asside className=" logo">
+            <a href="/" className="logo">
+            <img src={logo} alt="logo"/>
+            </a>
+        </asside>   
