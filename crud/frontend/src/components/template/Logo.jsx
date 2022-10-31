d39d6a5bd695d083650c609a42bf6c737550f542
NikolasDaltro <nikolasdsantos@gmail.com>
2022-10-31 17:58:21 -0300

    componente footer

--- crud/frontend/src/components/template/Logo.jsx
+++ crud/frontend/src/components/template/Logo.jsx
@@ -3,8 +3,8 @@ import logo from '../../assets/imgs/logo.png'
 import React from 'react'
 
 export default props =>
-        <asside className=" logo">
-            <a href="/" className="logo">
+        <asside className="logo">
+            <a href="/" className ="logo">
             <img src={logo} alt="logo"/>
             </a>
         </asside>   
